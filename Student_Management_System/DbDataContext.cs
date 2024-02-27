bc9a69cef5373df86d684d050e7fdbc72fc070e8
ACSENEVIRATHNA <acsenevirathne@gmail.com>
2024-02-27 14:42:59 +0530

    Commit
    Whole Project

---
+++ Student_Management_System/DbDataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Management_System.Models;
+
+namespace Student_Management_System
+{
+    public class DbDataContext : DbContext
+    {
+        //Change this path to your database full path
+        private readonly string _path = @"<Add your absolute path to database>";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source = {_path}");
+        }
+
+        public DbSet<User> DbUsers { get; set; }
+        public DbSet<Student> DbStudents { get; set; }
+        public DbSet<EnrolledModules> DbEnrolledModules { get; set; }
+        public DbSet<Models.Module> DbModules { get; set; }
+    }
+}
